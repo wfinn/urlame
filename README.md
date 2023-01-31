2154e706700da68b52111adee41bb76d9982d7e0
wfinn <no@mail>
2023-01-31 14:34:29 +0100

    Initial implementation of equivalences; making normalizeItem more generic (/foo2019 and /foo2018 are eq now)

--- README.md
+++ README.md
@@ -34,11 +34,15 @@ It further can detect some patters in parts of URLs. In the following examples, 
 | UUIDs | /id/123e4567-e89b-12d3-a456-426614174000 & /id/34c764cf-b13b-4d36-ab93-4474f5b91848|
 | common post titles | /common-post-title & /another-post-title |
 
-## Limitations
+## Equivalences
 
-Some websites have patterns which should be considered, but which do not apply for other targets, meaning `urlame` would filter stuff we do not want to filter out on these otehr websites.  
+Some websites have patterns which should be considered, but which do not apply for other targets, meaning `urlame` would filter stuff we do not want to filter out on these other websites.  
 In such cases, you must modify the source code yourself to get decent results.
 
+One mechanism explicitly for target specific filtering exists called "Equivalences".  
+These are words you can define, which are kind of equivalent, from our view.  
+For example, when filtering tesla URLs you could define `model-3`,`model-y` and so on,
+so only the first `%carmodel%-release` and `/api/foo/%carmodel%` URLs are printed.
 
 ---
 
--- main.go
+++ main.go
@@ -11,17 +11,26 @@ import (
 	"strings"
 )
 
-var numberregex = regexp.MustCompile("^\\d+(\\.\\d+)?$")
-var profilepageregex = regexp.MustCompile("(?i)/(u|user|profile|author|member|referral)s?/[^/]+/?$")
-var titleregex = regexp.MustCompile("^[A-Za-z0-9-.]+$")
+var numberregex = regexp.MustCompile("\\d+(\\.\\d+)?")
+var profilepageregex = regexp.MustCompile("(?i)/(u|user|profile|author|member|referral)s?/[^/]+/?")
+var titleregex = regexp.MustCompile("[A-Za-z0-9-.]+")
 var langregex = buildlangregex()
-var uuidregex = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
-
-var hashregex = regexp.MustCompile("^[a-zA-Z0-9]+$")
-var hashlens = []int{32, 40, 64, 128}
+var uuidregex = regexp.MustCompile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
+var hashregex = regexp.MustCompile("[a-zA-Z0-9]{32,40,64,128}")
+var equivalenceregexes = buildquivalences()
 
 var exts = []string{".css", ".png", ".jpg", ".jpeg", ".svg", ".gif", ".mp3", ".mp4", ".rss", ".ttf", ".woff", ".woff2", ".eot", ".pdf", ".m4v", ".ogv", ".webm"}
 var paths = []string{"wp-content", "blog", "blogs", "product", "doc", "docs", "support"}
+
+var langcodes = []string{"af", "af-ZA", "ar", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB", "ar-LY", "ar-MA", "ar-OM", "ar-QA", "ar-SA", "ar-SY", "ar-TN", "ar-YE", "az", "az-AZ", "az-AZ", "be", "be-BY", "bg", "bg-BG", "bs-BA", "ca", "ca-ES", "cs", "cs-CZ", "cy", "cy-GB", "da", "da-DK", "de", "de-AT", "de-CH", "de-DE", "de-LI", "de-LU", "dv", "dv-MV", "el", "el-GR", "en", "en-AU", "en-BZ", "en-CA", "en-CB", "en-GB", "en-IE", "en-JM", "en-NZ", "en-PH", "en-TT", "en-US", "en-ZA", "en-ZW", "eo", "es", "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-DO", "es-EC", "es-ES", "es-ES", "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PE", "es-PR", "es-PY", "es-SV", "es-UY", "es-VE", "et", "et-EE", "eu", "eu-ES", "fa", "fa-IR", "fi", "fi-FI", "fo", "fo-FO", "fr", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "fr-LU", "fr-MC", "gl", "gl-ES", "gu", "gu-IN", "he", "he-IL", "hi", "hi-IN", "hr", "hr-BA", "hr-HR", "hu", "hu-HU", "hy", "hy-AM", "id", "id-ID", "is", "is-IS", "it", "it-CH", "it-IT", "ja", "ja-JP", "ka", "ka-GE", "kk", "kk-KZ", "kn", "kn-IN", "ko", "ko-KR", "kok", "kok-IN", "ky", "ky-KG", "lt", "lt-LT", "lv", "lv-LV", "mi", "mi-NZ", "mk", "mk-MK", "mn", "mn-MN", "mr", "mr-IN", "ms", "ms-BN", "ms-MY", "mt", "mt-MT", "nb", "nb-NO", "nl", "nl-BE", "nl-NL", "nn-NO", "ns", "ns-ZA", "pa", "pa-IN", "pl", "pl-PL", "ps", "ps-AR", "pt", "pt-BR", "pt-PT", "qu", "qu-BO", "qu-EC", "qu-PE", "ro", "ro-RO", "ru", "ru-RU", "sa", "sa-IN", "se", "se-FI", "se-FI", "se-FI", "se-NO", "se-NO", "se-NO", "se-SE", "se-SE", "se-SE", "sk", "sk-SK", "sl", "sl-SI", "sq", "sq-AL", "sr-BA", "sr-BA", "sr-SP", "sr-SP", "sv", "sv-FI", "sv-SE", "sw", "sw-KE", "syr", "syr-SY", "ta", "ta-IN", "te", "te-IN", "th", "th-TH", "tl", "tl-PH", "tn", "tn-ZA", "tr", "tr-TR", "tt", "tt-RU", "ts", "uk", "uk-UA", "ur", "ur-PK", "uz", "uz-UZ", "uz-UZ", "vi", "vi-VN", "xh", "xh-ZA", "zh", "zh-CN", "zh-HK", "zh-MO", "zh-SG", "zh-TW", "zu", "zu-zA"}
+
+// Equivalences are explained in README.md
+// Modify to include target specific words, which can be normalized to reduce results
+// left side must contain a unique string of the pattern !-FOO-!, this is used internally as replacement
+var equivalences = map[string][]string{
+	//"!-TESLA-!" : {"model-3","model-y", "..."},
+	// langcodes are too small and need to be treated seperately
+}
 
 var dotstar = regexp.MustCompile(".*")
 var paramregexes = map[string]*regexp.Regexp{
@@ -31,14 +40,14 @@ var paramregexes = map[string]*regexp.Regexp{
 	"utm_content":  dotstar,
 	"utm_term":     dotstar,
 	"redirect":     regexp.MustCompile("no"),
+	//TODO version, v, cb, cache ...
 }
 
-var langcodes = []string{"af", "af-ZA", "ar", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB", "ar-LY", "ar-MA", "ar-OM", "ar-QA", "ar-SA", "ar-SY", "ar-TN", "ar-YE", "az", "az-AZ", "az-AZ", "be", "be-BY", "bg", "bg-BG", "bs-BA", "ca", "ca-ES", "cs", "cs-CZ", "cy", "cy-GB", "da", "da-DK", "de", "de-AT", "de-CH", "de-DE", "de-LI", "de-LU", "dv", "dv-MV", "el", "el-GR", "en", "en-AU", "en-BZ", "en-CA", "en-CB", "en-GB", "en-IE", "en-JM", "en-NZ", "en-PH", "en-TT", "en-US", "en-ZA", "en-ZW", "eo", "es", "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-DO", "es-EC", "es-ES", "es-ES", "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PE", "es-PR", "es-PY", "es-SV", "es-UY", "es-VE", "et", "et-EE", "eu", "eu-ES", "fa", "fa-IR", "fi", "fi-FI", "fo", "fo-FO", "fr", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "fr-LU", "fr-MC", "gl", "gl-ES", "gu", "gu-IN", "he", "he-IL", "hi", "hi-IN", "hr", "hr-BA", "hr-HR", "hu", "hu-HU", "hy", "hy-AM", "id", "id-ID", "is", "is-IS", "it", "it-CH", "it-IT", "ja", "ja-JP", "ka", "ka-GE", "kk", "kk-KZ", "kn", "kn-IN", "ko", "ko-KR", "kok", "kok-IN", "ky", "ky-KG", "lt", "lt-LT", "lv", "lv-LV", "mi", "mi-NZ", "mk", "mk-MK", "mn", "mn-MN", "mr", "mr-IN", "ms", "ms-BN", "ms-MY", "mt", "mt-MT", "nb", "nb-NO", "nl", "nl-BE", "nl-NL", "nn-NO", "ns", "ns-ZA", "pa", "pa-IN", "pl", "pl-PL", "ps", "ps-AR", "pt", "pt-BR", "pt-PT", "qu", "qu-BO", "qu-EC", "qu-PE", "ro", "ro-RO", "ru", "ru-RU", "sa", "sa-IN", "se", "se-FI", "se-FI", "se-FI", "se-NO", "se-NO", "se-NO", "se-SE", "se-SE", "se-SE", "sk", "sk-SK", "sl", "sl-SI", "sq", "sq-AL", "sr-BA", "sr-BA", "sr-SP", "sr-SP", "sv", "sv-FI", "sv-SE", "sw", "sw-KE", "syr", "syr-SY", "ta", "ta-IN", "te", "te-IN", "th", "th-TH", "tl", "tl-PH", "tn", "tn-ZA", "tr", "tr-TR", "tt", "tt-RU", "ts", "uk", "uk-UA", "ur", "ur-PK", "uz", "uz-UZ", "uz-UZ", "vi", "vi-VN", "xh", "xh-ZA", "zh", "zh-CN", "zh-HK", "zh-MO", "zh-SG", "zh-TW", "zu", "zu-zA"}
-
 func buildlangregex() *regexp.Regexp {
+	// langcodes are currently only completely matched, might remove ^ and $ for the longer ones?
 	reg := "(?i)^("
 	for i, lang := range langcodes {
-		reg += strings.Replace(lang, "-", "[-_]", 1)
+		reg += strings.Replace(lang, "-", "[-_]", 1) // match en-US & en_US
 		if i < len(langcodes) {
 			reg += "|"
 		}
@@ -47,10 +56,30 @@ func buildlangregex() *regexp.Regexp {
 	return regexp.MustCompile(reg)
 }
 
+func buildquivalences() map[string]*regexp.Regexp {
+	regexes := map[string]*regexp.Regexp{}
+	for replacement, eqwords := range equivalences {
+		regexes[replacement] = buildeqregex(eqwords)
+	}
+	return regexes
+}
+
+func buildeqregex(equivalentwords []string) *regexp.Regexp {
+	reg := "("
+	for i, lang := range langcodes {
+		reg += strings.Replace(lang, "-", "[-_]", 1) // match en-US & en_US
+		if i < len(langcodes) {
+			reg += "|"
+		}
+	}
+	reg += ")"
+	return regexp.MustCompile(reg)
+}
+
 func main() {
 	printNormalized := flag.Bool("print-normalized", false, "print the normalized version of the urls (for debugging)")
 	flag.Usage = func() {
-		fmt.Printf("cat urls.txt | %s [OPTIONS]\n", os.Args[0])
+		fmt.Printf("%s [OPTIONS] < urls.txt > less_urls.txt\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -117,6 +146,7 @@ func normalizeURL(urlstr string) string {
 		newvals := url.Values{}
 		for key := range u.Query() {
 			if !lameparam(key, u.Query().Get(key)) {
+				// ignoring lame params, if we see /foo and /foo?utm_source=bar we only list the first
 				newvals.Set(normalizeItem(key), "!-P-!")
 			}
 		}
@@ -143,9 +173,8 @@ func normalizePath(path string) string {
 		}
 		normalized += "/" + normalizeItem(part)
 	}
-
 	if fileName != "" { // this makes /foo/bar?x=y and /foo/bar/?x=y equivalent
-		// e.g. 123.json
+		// e.g. 123.json // FIXME: after the changes in normalizeItem are finished, this shouldn't needed anymore
 		lastInd := strings.LastIndex(fileName, ".")
 		if lastInd == -1 {
 			normalized += "/" + normalizeItem(fileName)
@@ -157,49 +186,18 @@ func normalizePath(path string) string {
 }
 
 func normalizeItem(item string) string {
-	// it's unlikely that we have urls with !-X-! in them which we would miss here
-	if numberregex.MatchString(item) {
-		return "!-N-!"
-	} else if postitle(item) {
+	if len(item) > 10 && titleregex.MatchString(item) {
 		return "!-T-!"
-	} else if hash(item) {
-		return "!-H-!"
-	} else if langcode(item) {
-		return "!-L-!"
-	} else if uuid(item) {
-		return "!-U-!"
+	}
+	orig := item
+	item = hashregex.ReplaceAllString(item, "!-H-!")
+	item = uuidregex.ReplaceAllString(item, "!-U-!")
+	item = langregex.ReplaceAllString(item, "!-L-!")
+	if orig == item {
+		// only apply `numberregex` if hash / UUID wasn't found, might be too generic otherwise
+		item = numberregex.ReplaceAllString(item, "!-N-!")
 	}
 	return item
-}
-
-func postitle(str string) bool {
-	if !titleregex.MatchString(str) {
-		return false
-	}
-	if len(str) > 10 {
-		return true
-	}
-	return strings.Count(str, "-") > 2
-}
-
-func hash(str string) bool {
-	if hashregex.MatchString(str) {
-		strlen := len(str)
-		for _, l := range hashlens {
-			if strlen == l {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func langcode(str string) bool {
-	return langregex.MatchString(str)
-}
-
-func uuid(str string) bool {
-	return uuidregex.MatchString(str)
 }
 
 func newURL(old *url.URL, path string, vals url.Values) string {
